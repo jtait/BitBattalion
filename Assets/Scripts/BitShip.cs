94b47c38b073711d62572d55348a2400d0e5273c
Jason <jwgtait@gmail.com>
2014-10-25 18:17:35 -0300

    scripting

---
+++ Assets/Scripts/BitShip.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BitShip : MonoBehaviour {
+
+    public float forwardVelocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+        print("moved");
+	}
+
+    void Move()
+    {
+        
+    }
+
+    void Shoot()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/BitShip_0.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BitShip_0 : BitShip {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    protected void Move()
+    {
+        rigidbody.velocity = new Vector3(0, forwardVelocity, 0);
+    }
+
+}
