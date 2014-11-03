51f4077b45b41a7cb27361ff7f5eefa8178fe78f
Jason <jwgtait@gmail.com>
2014-11-03 18:01:05 -0400

    added variables for future use

--- Assets/Scripts/BitShip.cs
+++ Assets/Scripts/BitShip.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public abstract class BitShip : MonoBehaviour {
 
     public float forwardVelocity;
+    public int difficulty;
+    public GameObject ammunition;
 
 	// Use this for initialization
 	void Start () {
