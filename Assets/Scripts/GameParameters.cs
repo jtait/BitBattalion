e9d592e40cd901b2222332861ab9c2ef673d83bb
Jason <jwgtait@gmail.com>
2014-11-07 17:29:55 -0400

    scripts

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -4,9 +4,29 @@ using System.Collections;
 public class GameParameters : MonoBehaviour {
 
     public int difficulty;
+    private long playerScore;
+    private const long maxScore = 999999999999L;
 
-    void start()
+    void Start()
     {
         difficulty = 1;
+        playerScore = 0;
     }
+
+    /* update the player's score - return true if score has changed */
+    public bool UpdateScore(long points)
+    {
+        long oldScore = playerScore;
+        playerScore += points;
+
+        return (playerScore != oldScore);
+
+    }
+
+    /* return the player's score */
+    public long getPlayerScore()
+    {
+        return playerScore;
+    }
+
 }
