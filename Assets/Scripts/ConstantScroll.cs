d37007834d2601caef91bd2aaeacf177c80bb5fd
Jason <jwgtait@gmail.com>
2014-11-17 13:24:30 -0400

    little efficiency tweak
    also helps keep the player frame in line

--- Assets/Scripts/ConstantScroll.cs
+++ Assets/Scripts/ConstantScroll.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class ConstantScroll : MonoBehaviour {
 
-    public float scrollSpeed = 1f;
+    public float scrollSpeed = 0.05f; // how fast the object moves in the y direction
+
+    private float xPos;
+    private float zPos;
 
 	// Use this for initialization
 	void Start () {
+        xPos = transform.position.x;
+        zPos = transform.position.z;
 	
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.position = new Vector3(transform.position.x, transform.position.y + scrollSpeed, transform.position.z);
+        transform.position = new Vector3(xPos, transform.position.y + scrollSpeed, zPos);
 	}
 
 }
