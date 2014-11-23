70b404848bab02c7fccc04612f7e3c41bc5cd19e
Jason <jwgtait@gmail.com>
2014-11-23 17:03:21 -0400

    start of first boss fight

---
+++ Assets/BossTube.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossTube : GenericBoss {
+
+    private const int BASE_HEALTH = 10; // the base health of the tube
+    private int allowableEscapedBits; // the number of bits that can escape before the player loses
+    private int escapedBits = 0;
+
+	protected override void Start () {
+        health = BASE_HEALTH * gParams.difficulty / 2;
+        allowableEscapedBits = 19; // arbitrary number
+	}
+
+    protected override void Update()
+    {
+        base.Update();
+        if (escapedBits > allowableEscapedBits)
+        {
+            LoseSequence();
+        }
+    }
+
+    protected override void DeathCheck()
+    {
+        base.DeathCheck();
+    }
+
+    protected override void DeathSequence()
+    {
+
+
+
+        // last thing
+        base.DeathSequence();
+    }
+
+    protected override void OnCollisionEnter(Collision col)
+    {
+        base.OnCollisionEnter(col);
+
+        if (col.collider.tag == "Enemy")
+        {
+            escapedBits++;
+        }
+    }
+
+    
+    private override void LoseSequence()
+    {
+
+    }
+
+}
---
+++ Assets/GenericBoss.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GenericBoss : MonoBehaviour {
+
+    protected int health;
+    protected GameParameters gParams;
+    protected const float WAIT_TIME_FOR_LEVEL_LOAD = 4;
+
+    void Awake()
+    {
+        gParams = GameObject.FindGameObjectWithTag("GameParameters").GetComponent<GameParameters>();
+    }
+
+	protected virtual void Start () {
+	
+	}
+	
+	protected virtual void Update () {
+        DeathCheck();
+	}
+
+    protected virtual void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag != "Weapon")
+        {
+            health--;
+        }
+    }
+
+    protected virtual void DeathCheck()
+    {
+        if (health <= 0)
+        {
+            DeathSequence();
+        }
+    }
+
+    /* called if the player wins and the boss dies */
+    protected virtual void DeathSequence()
+    {
+        /* override in child */
+
+        /* call base at the end of the death sequence */
+        StartCoroutine(WaitForLevelLoad(WAIT_TIME_FOR_LEVEL_LOAD));  
+    }
+
+    protected IEnumerator WaitForLevelLoad(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        // load next level here
+    }
+
+    /* called if the player loses the fight */
+    protected virtual void LoseSequence()
+    {
+
+    }
+}
