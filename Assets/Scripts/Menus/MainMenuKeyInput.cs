b1c01c6a0d3da9c2e77b7c32f40bddbf4b74074e
Jason <jwgtait@gmail.com>
2014-11-26 22:36:50 -0400

    menu tweaks
    now controllable by pressing keys in most cases

---
+++ Assets/Scripts/Menus/MainMenuKeyInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* handles keyboard input on main menu */
+public class MainMenuKeyInput : MonoBehaviour {
+
+    private const string CAMPAIGN = "Story_Level_01";
+    private const string ENDLESS = "Endless_Mode";
+    private const string HIGH_SCORE = "Menu_Main";
+    private const string OPTIONS = "Menu_Options";
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Application.LoadLevel(CAMPAIGN);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel(ENDLESS);
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            Application.LoadLevel(HIGH_SCORE);
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            Application.LoadLevel(OPTIONS);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+	}
+}
