6490546d9ab92dede0c80c74be1bed57d3356e43
Jason <jwgtait@gmail.com>
2014-11-26 22:25:52 -0400

    ONE SPLASH SCREEN TO RULE THEM ALL

---
+++ Assets/PressAnyKey.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressAnyKey : MonoBehaviour {
+
+    public string sceneToLoad;
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(sceneToLoad);
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Application.LoadLevel(sceneToLoad);
+        }
+    }
+}
---
+++ Assets/PulsingText.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PulsingText : MonoBehaviour {
+
+    private GUIText startText;
+    private bool fadeIn = false;
+
+	void Start () {
+        startText = gameObject.GetComponent<GUIText>();
+	}
+
+    void Update()
+    {
+        Pulse();
+    }
+
+    void FadeOut()
+    {
+        Color temp = startText.material.color;
+        temp.a = temp.a - (float)0.5 * Time.deltaTime;
+        startText.material.color = temp;
+    }
+
+    void FadeIn()
+    {
+        Color temp = startText.material.color;
+        temp.a = temp.a + (float)0.5 * Time.deltaTime;
+        startText.material.color = temp;
+    }
+
+    void Pulse()
+    {
+        if(guiText.material.color.a > 0 && !fadeIn)
+        {
+            FadeOut();
+        }
+        else
+            fadeIn = true;
+        if (guiText.material.color.a < 1 && fadeIn)
+        {
+            FadeIn();
+        }
+        else
+            fadeIn = false;
+    }
+
+}
