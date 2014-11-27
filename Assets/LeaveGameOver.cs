f311c47b7f3d926a3c39f3b17c0f9ff4a1167166
Jason <jwgtait@gmail.com>
2014-11-26 20:03:44 -0400

    placeholder game over screen

---
+++ Assets/LeaveGameOver.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaveGameOver : MonoBehaviour {
+
+    public string sceneToLoad;
+    GameParameters gParams;
+
+    void Awake()
+    {
+        try
+        {
+            gParams = GameObject.FindGameObjectWithTag("GameParameters").GetComponent<GameParameters>();
+        }
+        catch (System.NullReferenceException)
+        {
+            gParams = null;
+        }
+        
+    }
+
+    void OnMouseDown()
+    {
+        if(gParams != null)
+            Destroy(gParams.gameObject);
+        Application.LoadLevel(sceneToLoad);
+    }
+}
