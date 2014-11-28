c610adeaf7ae9668ecec5ee71ede21e8e55af063
Jason <jwgtait@gmail.com>
2014-11-28 18:31:42 -0400

    added bitship_2

---
+++ Assets/Scripts/Enemy/BitShip_2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BitShip_2 : GenericEnemy
+{
+
+    private Transform targetTransform;
+
+    protected override void Start()
+    {
+        targetTransform = GameObject.FindGameObjectWithTag("Boss").transform;
+
+        /* set all basic parameters */
+        health = 1;
+        pointValue = 200 * difficulty;
+        fireRate = fireRate * difficulty;
+        baseForwardVelocity = 3 * difficulty;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    /* override base class Move() function */
+    void Move()
+    {
+        rigidbody.velocity = Vector3.down * baseForwardVelocity;
+    }
+
+    protected override void Death()
+    {
+        base.Death();
+    }
+}
