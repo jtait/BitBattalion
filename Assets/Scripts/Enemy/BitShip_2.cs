7bfe7cefafe66fa84099131e129919e69904f494
Jason <jwgtait@gmail.com>
2014-11-28 19:27:18 -0400

    deleted unused variable

--- Assets/Scripts/Enemy/BitShip_2.cs
+++ Assets/Scripts/Enemy/BitShip_2.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class BitShip_2 : GenericEnemy
 {
 
-    private Transform targetTransform;
-
     protected override void Start()
     {
         targetTransform = GameObject.FindGameObjectWithTag("Boss").transform;
