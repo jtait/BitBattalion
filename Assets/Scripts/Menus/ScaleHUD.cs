f2f1e6f465949902b1aac4dbc03fd6ca85c514f8
Jason <jwgtait@gmail.com>
2014-11-28 21:40:46 -0400

    gui text scaled to screen resolution
    well that was easy...

---
+++ Assets/Scripts/Menus/ScaleHUD.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleHUD : MonoBehaviour {
+
+    public int SCALE_FACTOR;
+
+    private int screenWidth;
+
+    void Awake()
+    {
+        screenWidth = Screen.width;
+    }
+
+    void OnGUI()
+    {
+        guiText.fontSize = screenWidth / SCALE_FACTOR;
+    }
+}
