a50c29894b76a51a4b084069f72419a9930c69f8
Jason <jwgtait@gmail.com>
2014-12-02 22:42:43 -0400

    sound effects on powerup pickup

--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class Powerup : MonoBehaviour {
 
     public PowerUpType type;
+    private AudioClip pickupSound;
+
+    void Awake()
+    {
+        pickupSound = Resources.Load<AudioClip>("SoundFX/powerupPickup/59473__glewlio__melody-sound-15");
+    }
 
     void Start()
     {
@@ -15,6 +21,7 @@ public class Powerup : MonoBehaviour {
     {
         if (col.tag == "PlayerCollider")
         {
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position, 0.75f); // play explosion sound
             col.GetComponentInParent<PlayerControl>().PowerUpPickup(type); // trigger the powerup method on the player to assign a powerup
             Destroy(gameObject); // destroy the powerup object
         }
