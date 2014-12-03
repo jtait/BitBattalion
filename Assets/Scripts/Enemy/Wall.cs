cec79d7518661e819423dfd857b7f9365eade775
Jason <jwgtait@gmail.com>
2014-12-03 00:31:20 -0400

    fixed lasers not being destroyed on walls

---
+++ Assets/Scripts/Enemy/Wall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : GenericEnemy{
+
+    protected override void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Weapon")
+        {
+            Destroy(col.collider.gameObject); // destroy the weapon that hit the enemy
+        }
+    }
+}
