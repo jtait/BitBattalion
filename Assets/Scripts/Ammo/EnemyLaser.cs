a6b7740dec4311bb040169916c197b392b4d3f5b
jtait <jwgtait@gmail.com>
2015-06-17 22:16:46 -0300

    updated to unity version 5.1

--- Assets/Scripts/Ammo/EnemyLaser.cs
+++ Assets/Scripts/Ammo/EnemyLaser.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class EnemyLaser : GenericAmmo {
 
-    AudioClip pewpew;
+    AudioClip laserSound;
+    private const float soundVolume = 0.05f;
 
     void Awake()
     {
-        pewpew = Resources.Load<AudioClip>("SoundFX/enemyLaser/35683__jobro__laser6");
+        laserSound = Resources.Load<AudioClip>("SoundFX/enemyLaser/35683__jobro__laser6");
     }
 
     protected override void Start()
     {
         base.Start();
-        AudioSource.PlayClipAtPoint(pewpew, transform.position, 1f);
+        AudioSource.PlayClipAtPoint(laserSound, transform.position, soundVolume);
     }
 
     protected override void FixedUpdate()
